e82a4d8ca48de1a8312e577a64b86f8d6450f784
Yves Sagnieres <sagnieres.yves@gmail.com>
2018-09-21 23:42:15 -0400

    root route to -> idea index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  	root :to => "ideas#index"
+
 	resources :ideas, only: [:index, :show, :new, :create, :edit, :update, :destroy]
 
 	get '/upvote/:id', to: 'ideas#upvote', as: :upvote
