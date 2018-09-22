5d40a8dbc6bbdba93ff544ff0ca9ebd71e8c0ad0
Yves Sagnieres <sagnieres.yves@gmail.com>
2018-09-21 23:13:53 -0400

    adding postgre

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_09_21_215903) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "ideas", force: :cascade do |t|
     t.string "title"
     t.text "text"
